e303422fb9157135a6bb9e0462f0b428e87471c4
DorsafH <148040990+DorsafH@users.noreply.github.com>
2023-12-06 22:07:52 +0100

    Add files via upload

--- Client.java
+++ Client.java
@@ -4,25 +4,79 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class Client extends Utilisateurs {
+	 int compteur=0;
 	   private int idClient;
+	   private int idUtilisateur;
 	    private String nomClient;
 	    private String tel;
 	    private String email;
 	    private String motDePasse;
 	    public Location location;
 	    private Connection connection;
+	     static String url = "jdbc:mysql://localhost/luxedrive";
+	   static   String user = "root";
+	    static  String passwd = "root";
+	    public Client (int id,String n, String t,String email, String mdp) {
+	    	this.email=email;
+	    	this.motDePasse=mdp;
+	    	this.nomClient=n;
+	    	this.tel=t;
+	    	this.idClient=id;
+	    	this.compteur++;
+
+                       
+	    }  
+	    
+	    public void insererClient() {
+	       
+
+	        try (Connection connection = DriverManager.getConnection(url, user, passwd)) {
+	            String SQL = "INSERT INTO `client`(`idClient`, `nomClient`, `tel`, `email`, `motDePasse`, `idUtilisateur`) " +
+	                         "VALUES (?, ?, ?, ?, ?, NULL)";
+
+	            // Création de la requête préparée
+	            try (PreparedStatement statement = connection.prepareStatement(SQL)) {
+	                // Définition des valeurs à insérer
+	                statement.setInt(1, this.idClient); 
+	                statement.setString(2, this.nomClient); 
+	                statement.setString(3, this.tel); 
+	                statement.setString(4, this.email);
+	                statement.setString(5, this.motDePasse); 
+	                //statement.setNull(idUtilisateur, compteur); 
+
+	                // Exécution de la requête
+	                int rowsAffected = statement.executeUpdate();
+	                if (rowsAffected > 0) {
+	                    System.out.println("Données insérées avec succès !");
+	                } else {
+	                    System.out.println("Aucune donnée n'a été insérée.");
+	                }
+	            }
+	        } catch (Exception exception) {
+	            throw new RuntimeException(exception);
+	        }
+	    }
+	
+	        
+	    
+	       
 	   
 	    public boolean ConnexionClient(String email, String motDePasse) {
-	    	    try {
+	    	    try (Connection connection = DriverManager.getConnection(url, user, passwd)) {
 	    	        String query = "SELECT * FROM client WHERE email = ? AND motDePasse = ?";
 	    	        PreparedStatement statement = connection.prepareStatement(query);
 	    	        statement.setString(1, email);
 	    	        statement.setString(2, motDePasse);
 	    	        ResultSet resultSet = statement.executeQuery();
-	    	        return resultSet.next(); // Si le client existe avec ces identifiants
+	    	        String updateQuery = "UPDATE client SET estConnecte = true WHERE email = ?";
+	    	        PreparedStatement statement2 = connection.prepareStatement(updateQuery);
+	    	        return resultSet.next(); 
+	    	   
 	    	    } catch (Exception e) {
 	    	        e.printStackTrace();
 	    	    }
@@ -30,11 +84,22 @@ public class Client extends Utilisateurs {
 	    }
 	    public void miseJourInfoClient() {
 	    }
-	    public void DeconnexionClient() {
-	    	disconnect();
-	     }
-	    
+	  
+	    	public boolean deconnexionClient() {
+	    	    try (Connection connection = DriverManager.getConnection(url, user, passwd)) {
+	    	        String updateQuery = "UPDATE client SET estConnecte = ? WHERE email = ?";
+	    	        PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+	    	        updateStatement.setBoolean(1, false); // Marquer le client comme déconnecté
+	    	        updateStatement.setString(2, this.email);
 
+	    	        int rowsAffected = updateStatement.executeUpdate();
+	    	        return rowsAffected > 0; // Si au moins une ligne a été affectée, la déconnexion a réussi
+	    	    } catch (SQLException e) {
+	    	        e.printStackTrace();
+	    	    }
+	    	    return false;
+	    	}	    	
+	    	
 	    public String getNomClient() {
 			return nomClient;
 		}
@@ -72,11 +137,11 @@ public class Client extends Utilisateurs {
 			this.location = location;
 		}
 		
-		public void SupprimerCompte(Client client) {
-	        try {
-	            String query = "DELETE FROM clients WHERE id = ?";
+		public void SupprimerCompte() {
+	        try(Connection connection = DriverManager.getConnection(url, user, passwd)) {
+	            String query = "DELETE FROM `client` WHERE `idClient`= ?";
 	            PreparedStatement statement = connection.prepareStatement(query);
-	            statement.setInt(1, client.getIdClient()); // Supposons que getId() retourne l'identifiant du client
+	            statement.setInt(1, this.getIdClient()); 
 	            statement.executeUpdate();
 	            System.out.println("Compte client supprimé !");
 	        } catch (Exception e) {
